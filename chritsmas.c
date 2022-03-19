7de369325397ba2fdcead307b274630ecbd2bcb2
shiabsararrafid <shrafid.532@gmail.com>
2022-03-20 04:37:05 +0600

    chritsmas problem solved

---
+++ chritsmas.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+
+    int d;
+    scanf("%d", &d);
+    switch (d)
+    {
+    case 22:
+        printf("Christmas Eve Eve Eve\n");
+        break;
+    case 23:
+        printf("Christmas Eve Eve\n");
+        break;
+    case 24:
+        printf("Christmas Eve\n");
+        break;
+    case 25:
+        printf("Christmas\n");
+        break;
+
+    default:
+        break;
+    }
+
+    return 0;
+}
